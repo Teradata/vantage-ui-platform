a77770d6d6a7845093b91233c967ace42a741153
Ed Morales <eduardo.morales@teradata.com>
2019-10-11 09:13:57 -0700

    feat(): add access module with initial guards (#18)

--- src/docs-ui/app/app.routes.ts
+++ src/docs-ui/app/app.routes.ts
@@ -3,10 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 
 import { VantageAuthenticationGuard } from '@td-vantage/ui-platform/auth';
+import { VantageBlockRootAccessGuard, VantageBlockUserAccessGuard } from '@td-vantage/ui-platform/access';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'login', component: MainComponent, canActivate: [ VantageAuthenticationGuard ] },
+  { path: 'block-root', component: MainComponent, canActivate: [ VantageBlockRootAccessGuard ] },
+  { path: 'block-user', component: MainComponent, canActivate: [ VantageBlockUserAccessGuard ] },
   { path: '**', redirectTo: '/', },
 ];
 
