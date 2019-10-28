1ba896205c2f0dffb1dc32476419a4aa8e2bcfa5
Christian Memije <christianmemije@users.noreply.github.com>
2019-10-28 16:37:31 -0700

    fix(cypress): allow login to work in electron and reduce visits (#19)
    * fix(cypress): allow login to work in electron and reduce visits
    
    * fix(npm-audit): fix npm audit issues

--- src/lib/testing/cypress/cypress.ts
+++ src/lib/testing/cypress/cypress.ts
@@ -10,37 +10,37 @@ export interface ILoginCredentials {
 
 // inspired by https://vrockai.github.io/blog/2017/10/28/cypress-keycloak-intregration/
 export function login({ username, password }: ILoginCredentials): void {
-  cy.visit(BASE_URL);
-  cy.url().should('include', LOGIN_URL);
   cy.request({
     url: LOGIN_URL,
   }).then((response: any) => {
     const loginPageHtml: HTMLElement = document.createElement('html');
     loginPageHtml.innerHTML = response.body;
     const loginForm: HTMLFormElement = loginPageHtml.querySelector('#kc-form-login');
-    if (!loginForm) {
-      return;
+    if (loginForm) {
+      cy.request({
+        form: true,
+        method: 'POST',
+        url: loginForm.action,
+        followRedirect: false,
+        body: {
+          username,
+          password,
+        },
+      }).then(() => {
+        _redirectToHome();
+      });
+    } else {
+      _redirectToHome();
     }
-    cy.request({
-      form: true,
-      method: 'POST',
-      url: loginForm.action,
-      followRedirect: false,
-      body: {
-        username: username,
-        password: password,
-      },
-    }).then(() => {
-      cy.visit(BASE_URL);
-      cy.url().should('not.include', LOGIN_URL);
-      cy.url().should('include', BASE_URL);
-    });
   });
 }
 
 export function logout(): void {
+  cy.request('/api/user/logout');
+}
+
+function _redirectToHome(): void {
   cy.visit(BASE_URL);
-  cy.visit('/api/user/logout');
-  cy.visit(BASE_URL);
-  cy.url().should('include', LOGIN_URL);
+  cy.url().should('not.include', LOGIN_URL);
+  cy.url().should('include', BASE_URL);
 }
