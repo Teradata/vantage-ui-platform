d2af13dd05259502afca1c727141584cf4d1bc28
pd250030 <phillip.dominguez@teradata.com>
2020-01-15 08:21:34 -0800

    feat: ui platform testbed

--- src/docs-ui/app/app.module.ts
+++ src/docs-ui/app/app.module.ts
@@ -6,12 +6,36 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { CovalentCommonModule } from '@covalent/core/common';
 import { CovalentLayoutModule } from '@covalent/core/layout';
@@ -28,7 +52,7 @@ import { VantageAuthenticationModule } from '@td-vantage/ui-platform/auth';
 import { VantageAccessModule } from '@td-vantage/ui-platform/access';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
+import { MainComponent, DialogContentComponent } from './main/main.component';
 
 import { appRoutes, appRoutingProviders } from './app.routes';
 
@@ -37,7 +61,7 @@ import { TranslateModule, TranslateService, TranslateLoader } from '@ngx-transla
 import { getSelectedLanguage, getSelectedLocale, createTranslateLoader, SUPPORTED_LANGS } from '../config/translate';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent], // directives, components, and pipes owned by this NgModule
+  declarations: [AppComponent, MainComponent, DialogContentComponent], // directives, components, and pipes owned by this NgModule
   imports: [
     /** Angular Modules */
     HttpClientModule,
@@ -47,12 +71,36 @@ import { getSelectedLanguage, getSelectedLocale, createTranslateLoader, SUPPORTE
     BrowserModule,
     BrowserAnimationsModule,
     /** Material Modules */
+    MatCheckboxModule,
     MatButtonModule,
-    MatIconModule,
-    MatCardModule,
     MatInputModule,
-    MatSnackBarModule,
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
     /** Covalent Modules */
     CovalentCommonModule,
     CovalentLayoutModule,
--- src/docs-ui/app/main/main.component.ts
+++ src/docs-ui/app/main/main.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-import { VantageToastService, VantageErrorService } from '@td-vantage/ui-platform/utilities';
-import { VantageSessionService } from '@td-vantage/ui-platform/auth';
-import { IUser } from '@td-vantage/ui-platform/user';
-import { timeout } from 'rxjs/operators';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'td-main',
@@ -11,42 +8,116 @@ import { timeout } from 'rxjs/operators';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-  user: IUser;
-  loggedIn: boolean = false;
+  DARK_THEME: string = 'dark-theme';
+  LIGHT_THEME: string = 'light-theme';
 
-  constructor(
-    private _errorService: VantageErrorService,
-    private _toastService: VantageToastService,
-    private _sessionService: VantageSessionService,
-  ) {}
+  isDarkTheme = false;
+  lastDialogResult: string;
+  mode: string;
+  value: number;
 
-  async ngOnInit(): Promise<void> {
-    try {
-      this.user = await this._sessionService
-        .getInfo()
-        .pipe(timeout(5000))
-        .toPromise();
-      this.loggedIn = true;
-    } catch (error) {
-      if (error.status !== 401) {
-        this.loggedIn = false;
-        this._errorService.open(error);
-      }
+  foods: any[] = [
+    { name: 'Pizza', rating: 'Excellent' },
+    { name: 'Burritos', rating: 'Great' },
+    { name: 'French fries', rating: 'Pretty good' },
+  ];
+
+  public selectedValue: string;
+
+  public games = [
+    { value: 'rts-0', viewValue: 'Starcraft' },
+    { value: 'rpg-1', viewValue: "Baldur's Gate" },
+    { value: 'fps-2', viewValue: 'Doom' },
+  ];
+
+  public progress = 0;
+  public slider = {
+    autoTicks: false,
+    disabled: false,
+    invert: false,
+    max: 100,
+    min: 0,
+    showTicks: false,
+    step: 1,
+    thumbLabel: false,
+    value: 0,
+    vertical: false,
+    tickInterval: 1,
+    checked: true,
+  };
+  public tiles = [
+    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+  ];
+
+  public color: string;
+
+  public availableColors = [
+    { name: 'none', color: '' },
+    { name: 'Primary', color: 'primary' },
+    { name: 'Accent', color: 'accent' },
+    { name: 'Warn', color: 'warn' },
+  ];
+
+  constructor(private _dialog: MatDialog, private _snackbar: MatSnackBar) {
+    // Update the value for the progress-bar on an interval.
+    setInterval(() => {
+      this.progress = (this.progress + Math.floor(Math.random() * 4) + 1) % 100;
+    }, 200);
+  }
+
+  ngOnInit(): void {
+    // Sets default theme to dark mode if user never set theme
+    if (!this.activeTheme) {
+      this.theme(this.DARK_THEME);
     }
   }
 
-  openToast(): void {
-    this._toastService.open('My toast');
+  get activeTheme(): string {
+    return localStorage.getItem('vantage.theme');
+  }
+  theme(theme?: string): void {
+    localStorage.setItem('vantage.theme', theme);
+    document.getElementsByTagName('body').item(0).className = theme;
   }
 
-  openDialog(): void {
-    this._errorService.open({
-      error: 1000,
-      message: 'Error message',
+  openDialog() {
+    const dialogRef = this._dialog.open(DialogContentComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      this.lastDialogResult = result;
     });
   }
 
-  public logout(): void {
-    this._sessionService.logout();
+  showSnackbar() {
+    this._snackbar.open('YUM SNACKS', 'CHEW');
   }
+  get tickInterval(): number | 'auto' {
+    return this.slider.showTicks ? (this.slider.autoTicks ? 'auto' : this.slider.tickInterval) : null;
+  }
+  set tickInterval(v) {
+    this.slider.tickInterval = Number(v);
+  }
+}
+
+@Component({
+  template: `
+    <h1 matDialogTitle>This is a dialog</h1>
+    <div matDialogContent>
+      <mat-form-field>
+        <label>
+          This is a text box inside of a dialog.
+          <input matInput #dialogInput />
+        </label>
+      </mat-form-field>
+    </div>
+    <div matDialogActions>
+      <button mat-button [matDialogClose]="dialogInput.value">CLOSE</button>
+    </div>
+  `,
+})
+export class DialogContentComponent {
+  constructor(public dialogRef: MatDialogRef<DialogContentComponent>) {}
 }
