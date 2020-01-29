52f68783812e744988ad0618b5f44549133431cf
Phil Dominguez <felips.110@gmail.com>
2020-01-29 14:29:48 -0800

    refactor(): theming structure (#32)
    * fix: background set to bgc color
    
    * refactor: theming strucutre
    
    * fix: adding directory to lib build
    
    * feat: ui platform testbed
    
    * Split light and dark theme variables out. Apply various background color overrides.
    
    * Update backdrop name to overlay
    
    * fix: remove duplicate selector
    
    * feat: typography
    
    * feat: adding github link
    
    * fix: set up font declaration so it respects CSS weight properties
    
    * fix: add caption type style
    
    * feat: adding fallbacks and overline
    
    * fix: removing unnecesary file and cleaning up
    
    * feat: background map, cleanup, & ninja styling
    
    * fix: typography naming
    
    * feat: add more custom colors to theme maps
    
    * fix: update styling on buttons
    
    * fix: update base button colors
    
    * feat: dial in colors for chips
    
    * feat: add other input types for testing
    
    * fix: programmatically feed color values through to color-specific maps
    
    * fix: remove extraneous color maps for states
    
    * fix: border on outline buttons should respect disabled state
    
    * feat: expand td-slate palette for better contrast
    
    * feat(): remove roboto and small layout fix
    
    * feat(pre-loader): pull in new theme variables
    
    Co-authored-by: Christian Memije <christianmemije@users.noreply.github.com>

--- src/lib/utilities/pre-loader/index.js
+++ src/lib/utilities/pre-loader/index.js
@@ -5,11 +5,15 @@ const path = require('path');
 const sass = require('sass');
 
 module.exports = (targetOptions, originalIndexHtml) => {
-  const preLoaderHtml = fs.readFileSync(path.join(__dirname, 'pre-loader.html'), { encoding: 'utf8' }).toString();
-  const preLoaderCss = sass
-    .renderSync({ file: path.join(__dirname, 'pre-loader.scss'), includePaths: ['node_modules'] })
-    .css.toString();
-  const preLoaderHtmlWithCss = preLoaderHtml.replace('<style></style>', `<style>${preLoaderCss}</style>`);
-  const newIndexHtml = originalIndexHtml.replace('<div id="td-pre-loader"></div>', preLoaderHtmlWithCss);
-  return newIndexHtml;
+  try {
+    const preLoaderHtml = fs.readFileSync(path.join(__dirname, 'pre-loader.html'), { encoding: 'utf8' }).toString();
+    const preLoaderCss = sass
+      .renderSync({ file: path.join(__dirname, 'pre-loader.scss'), includePaths: ['node_modules'] })
+      .css.toString();
+    const preLoaderHtmlWithCss = preLoaderHtml.replace('<style></style>', `<style>${preLoaderCss}</style>`);
+    const newIndexHtml = originalIndexHtml.replace('<div id="td-pre-loader"></div>', preLoaderHtmlWithCss);
+    return newIndexHtml;
+  } catch (error) {
+    return new Error(error);
+  }
 };
