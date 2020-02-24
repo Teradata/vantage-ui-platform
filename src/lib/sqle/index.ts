f2d59ce163c7af3a294017d7175da7b66af187b9
Austin Tinsley <austin.tinsley@teradata.com>
2020-02-24 15:03:43 -0500

    feat(sqle): add spooled query service

--- src/lib/sqle/index.ts
+++ src/lib/sqle/index.ts
@@ -1,4 +1,5 @@
 export * from './query.service';
+export * from './spooled-query.service';
 export * from './dictionary.service';
 export * from './connection.service';
 export * from './sqle.module';
