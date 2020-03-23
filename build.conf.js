2e5889959062a7bb6c731630f029fe7cabd36530
emoralesb05 <eduardo.morales@teradata.com>
2020-03-23 10:25:20 -0700

    feat(): generate changelog and bump to version v1.0.0-beta.1

--- build.conf.js
+++ build.conf.js
@@ -4,6 +4,6 @@ module.exports = {
   deployed: 'deploy/ui-platform/',
   angularVersion: '9.0.0',
   materialVersion: '9.0.0',
-  covalentVersion: '3.0.0-0',
+  covalentVersion: '3.0.0',
   translateVersion: '12.0.0',
 };
