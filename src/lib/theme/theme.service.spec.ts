395b21c48a840ad1a5b26f8f71991e0b97920400
oseer <oseer.williams@teradata.com>
2020-03-25 12:30:33 -0400

    feat(theme-service): added more tests for coverage

--- src/lib/theme/theme.service.spec.ts
+++ src/lib/theme/theme.service.spec.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { THEME_LOCAL_STORAGE_KEY, VantageThemeService, VantageTheme, VANTAGE_THEME_PROVIDER } from './theme.service';
+import { Observable } from 'rxjs';
 
 let mockItems = {};
 const mockLocalStorage = {
@@ -31,6 +32,9 @@ describe('ThemeService', () => {
 
     mediaSpy = spyOn(window, 'matchMedia').and.returnValue(mediaQueryList);
 
+    // ensure browser under test is always the same
+    mediaSpy.and.returnValue({ matches: true });
+
     TestBed.configureTestingModule({
       imports: [CommonModule],
       providers: [VANTAGE_THEME_PROVIDER],
@@ -39,53 +43,147 @@ describe('ThemeService', () => {
 
   afterEach(resetMockItems);
 
-  it('should pull previous theme choices from localStorage', (done: DoneFn) => {
+  it('should be able map values based on active theme', fakeAsync(() => {
+    service = TestBed.inject(VantageThemeService);
+
+    const mapObs: Observable<any> = service.map({ 'dark-theme': 'hacker vibes 👩‍💻', 'light-theme': 'miami vibes 🏖️' });
+    let mapResult: string;
+
+    service.applyDarkTheme();
+
+    mapObs.subscribe((map: any) => {
+      mapResult = map;
+    });
+
+    tick(50);
+
+    expect(mapResult).toEqual('hacker vibes 👩‍💻');
+
+    service.applyLightTheme();
+
+    tick(50);
+
+    expect(mapResult).toEqual('miami vibes 🏖️');
+  }));
+
+  it('should be able to give fallback from map values based on active theme', fakeAsync(() => {
+    service = TestBed.inject(VantageThemeService);
+
+    const mapObs: Observable<any> = service.map({ 'dark-theme': 'hacker vibes 👩‍💻' }, 'fallback');
+    let mapResult: string;
+
+    service.applyLightTheme();
+
+    mapObs.subscribe((map: any) => {
+      mapResult = map;
+    });
+
+    tick(50);
+
+    expect(mapResult).toEqual('fallback');
+  }));
+
+  it('should be able to apply light theme', fakeAsync(() => {
+    service = TestBed.inject(VantageThemeService);
+
+    service.applyLightTheme();
+
+    tick(50);
+
+    expect(service.activeTheme()).toBe(VantageTheme.LIGHT);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  }));
+
+  it('should be able to apply dark theme', fakeAsync(() => {
+    service = TestBed.inject(VantageThemeService);
+
+    service.applyDarkTheme();
+
+    tick(50);
+
+    expect(service.activeTheme()).toBe(VantageTheme.DARK);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  }));
+
+  it('should pull previous theme choices from localStorage', fakeAsync(() => {
     // start with preloaded localStorage theme
     localStorage.setItem(THEME_LOCAL_STORAGE_KEY, VantageTheme.DARK);
+    const storageEvent: StorageEvent = new StorageEvent('storage', {
+      key: THEME_LOCAL_STORAGE_KEY,
+      newValue: VantageTheme.DARK,
+    });
 
     service = TestBed.inject(VantageThemeService);
-    service.activeTheme$.subscribe((theme: VantageTheme) => {
-      expect(localStorage.getItem).toHaveBeenCalled();
-      expect(mediaSpy).not.toHaveBeenCalled();
-      expect(theme).toBe(VantageTheme.DARK);
-      done();
-    });
-  });
 
-  it('should fall back to the browser theme preference', (done: DoneFn) => {
+    window.dispatchEvent(storageEvent);
+
+    tick(50);
+
+    expect(localStorage.getItem).toHaveBeenCalled();
+    expect(mediaSpy).not.toHaveBeenCalled();
+    expect(service.activeTheme()).toBe(VantageTheme.DARK);
+  }));
+
+  it('should check OS preferences when a storage event occurs if newValue is undefined', fakeAsync(() => {
+    // start with preloaded localStorage theme
+    localStorage.setItem(THEME_LOCAL_STORAGE_KEY, undefined);
+    const storageEvent: StorageEvent = new StorageEvent('storage', {
+      key: THEME_LOCAL_STORAGE_KEY,
+      newValue: undefined,
+    });
+
     // ensure browser under test is always the same
-    mediaSpy.and.returnValue({ matches: true });
-
-    service = TestBed.inject(VantageThemeService);
-
-    service.activeTheme$.subscribe((theme: VantageTheme) => {
-      expect(localStorage.getItem).toHaveBeenCalled();
-      expect(mediaSpy).toHaveBeenCalled();
-      expect(theme).toBe(VantageTheme.DARK);
-      done();
-    });
-  });
-
-  it('should default to the Light theme in absence of other preferences', (done: DoneFn) => {
-    service = TestBed.inject(VantageThemeService);
-
     mediaSpy.and.returnValue({ matches: false });
 
-    service.activeTheme$.subscribe((theme: VantageTheme) => {
-      expect(theme).toBe(VantageTheme.LIGHT);
-      done();
-    });
+    service = TestBed.inject(VantageThemeService);
+
+    window.dispatchEvent(storageEvent);
+
+    spyOn(service as any, 'checkOSPreference');
+
+    tick(50);
+
+    expect(mediaSpy).toHaveBeenCalled();
+
+    expect(service.activeTheme()).toBe(VantageTheme.LIGHT);
+  }));
+
+  it('should fall back to the browser theme preference', () => {
+    service = TestBed.inject(VantageThemeService);
+
+    expect(localStorage.getItem).toHaveBeenCalled();
+    expect(mediaSpy).toHaveBeenCalled();
+    expect(service.activeTheme()).toBe(VantageTheme.DARK);
   });
 
-  it('should set the theme in localStorage', (done: DoneFn) => {
+  it('should default to the Light theme in absence of other preferences', () => {
+    mediaSpy.and.returnValue({ matches: false });
+    service = TestBed.inject(VantageThemeService);
+
+    expect(service.activeTheme()).toBe(VantageTheme.LIGHT);
+  });
+
+  it('should set the theme in localStorage', () => {
     service = TestBed.inject(VantageThemeService);
     service.applyDarkTheme();
 
-    service.activeTheme$.subscribe((theme: VantageTheme) => {
-      expect(localStorage.setItem).toHaveBeenCalledWith(THEME_LOCAL_STORAGE_KEY, VantageTheme.DARK);
-      expect(theme).toBe(VantageTheme.DARK);
-      expect(localStorage.getItem(THEME_LOCAL_STORAGE_KEY)).toBe(VantageTheme.DARK);
-      done();
-    });
+    expect(localStorage.setItem).toHaveBeenCalledWith(THEME_LOCAL_STORAGE_KEY, VantageTheme.DARK);
+    expect(service.activeTheme()).toBe(VantageTheme.DARK);
+    expect(localStorage.getItem(THEME_LOCAL_STORAGE_KEY)).toBe(VantageTheme.DARK);
+  });
+
+  it('should toggle between light and dark', () => {
+    service = TestBed.inject(VantageThemeService);
+    service.applyDarkTheme();
+
+    expect(service.activeTheme()).toBe(VantageTheme.DARK);
+
+    service.toggleTheme();
+
+    expect(service.activeTheme()).toBe(VantageTheme.LIGHT);
+
+    service.toggleTheme();
+
+    expect(service.activeTheme()).toBe(VantageTheme.DARK);
   });
 });
