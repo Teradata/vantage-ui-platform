9677867be185157f007b9504a7bcc6c5f650f940
Lennan, Thomas R <Thomas.Lennan@Teradata.com>
2020-08-04 12:20:24 -0700

    feat(querybands): converted to map

--- src/lib/sqle/query.service.ts
+++ src/lib/sqle/query.service.ts
@@ -74,6 +74,13 @@ export enum QueryResultColumnTypes {
   'VARIANT_TYPE' = 'VARIANT_TYPE',
 }
 
+export interface IQueryBands {
+  ApplicationName: string;
+  Version: string;
+  ClientUser?: string;
+  [name: string]: string;
+}
+
 export interface IQueryPayload {
   query: string;
   session?: string;
@@ -86,7 +93,7 @@ export interface IQueryPayload {
   outputNumbersAsStrings?: boolean;
   spooledResultSet?: boolean;
   clientId?: string;
-  queryBands?: string;
+  queryBands?: IQueryBands;
 }
 
 export interface IQueryResultSet {
@@ -130,7 +137,7 @@ export interface ISessionPayload {
   charSet: string;
   defaultDatabase?: string;
   logMech?: string;
-  queryBands?: string;
+  queryBands?: IQueryBands;
 }
 
 @Injectable()
