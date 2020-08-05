8605079129d5ec6544870c427151b7f53059ba02
pd250030 <phillip.dominguez@teradata.com>
2020-08-05 11:52:04 -0700

    fix: alphabetize typed icons and icon array

--- src/lib/theme/utilities/icons.ts
+++ src/lib/theme/utilities/icons.ts
@@ -1,88 +1,88 @@
 export const TD_ICONS: TdIcon[] = [
-  'undock',
-  'dock_right',
-  'no_results',
   'alias_arrow',
   'api',
   'application',
   'bucket',
   'cluster_analysis',
-  'connected_identity',
   'column',
+  'connected_identity',
   'data_preparation',
-  'database',
+  'data_source',
   'database_changed',
   'database_edit',
   'database_foreign',
   'database_lab',
   'database_search',
   'database_synced',
-  'data_source',
+  'database',
   'disconnect',
+  'dock_right',
   'engine',
-  'formula',
-  'file',
   'file_changed',
   'file_edit',
   'file_foreign',
   'file_lab',
   'file_search',
   'file_synced',
+  'file',
+  'formula',
   'lab',
   'loader_dots',
   'machine_learning',
-  'model',
   'model_configuration',
+  'model',
+  'no_results',
   'node_branch',
   'node_merge',
   'object_storage',
   'operations',
   'operators_divide',
   'operators_equals',
-  'operators_greater_than',
   'operators_greater_than_or_equal',
-  'operators_less_than',
+  'operators_greater_than',
   'operators_less_than_or_equal',
+  'operators_less_than',
   'operators_multiply',
   'operators_not_equal',
-  'operators_parenthesis',
   'operators_parenthesis_left',
   'operators_parenthesis_right',
+  'operators_parenthesis',
   'operators_subtract',
+  'outbound_campaign_manager',
   'product_analyst',
   'product_console',
   'product_editor',
   'product_explorer',
-  'outbound_campaign_manager',
   'resize_northeast',
   'resize_northwest',
   'resize_southeast',
   'resize_southwest',
-  'rules',
   'rules_insert',
   'rules_select',
-  'script',
+  'rules',
   'script_macro',
   'script_sql',
   'script_stored_procedure',
+  'script',
   'segmentation',
-  'server',
   'server_changed',
   'server_edit',
   'server_foreign',
   'server_lab',
   'server_search',
   'server_synced',
+  'server',
   'sftp',
-  'table',
   'table_changed',
   'table_edit',
   'table_foreign',
   'table_lab',
   'table_search',
   'table_synced',
+  'table',
   'teradata',
   'text_analysis',
+  'undock',
   'variable',
   'view_sankey',
   'wand',
@@ -95,82 +95,82 @@ export type TdIcon =
   | 'application'
   | 'bucket'
   | 'cluster_analysis'
-  | 'connected_identity'
   | 'column'
+  | 'connected_identity'
   | 'data_preparation'
-  | 'database'
+  | 'data_source'
   | 'database_changed'
   | 'database_edit'
   | 'database_foreign'
   | 'database_lab'
   | 'database_search'
   | 'database_synced'
-  | 'data_source'
+  | 'database'
   | 'disconnect'
   | 'dock_right'
   | 'engine'
-  | 'formula'
-  | 'file'
   | 'file_changed'
   | 'file_edit'
   | 'file_foreign'
   | 'file_lab'
   | 'file_search'
   | 'file_synced'
+  | 'file'
+  | 'formula'
   | 'lab'
   | 'loader_dots'
   | 'machine_learning'
-  | 'model'
   | 'model_configuration'
+  | 'model'
+  | 'no_results'
   | 'node_branch'
   | 'node_merge'
-  | 'no_results'
   | 'object_storage'
   | 'operations'
   | 'operators_divide'
   | 'operators_equals'
-  | 'operators_greater_than'
   | 'operators_greater_than_or_equal'
-  | 'operators_less_than'
+  | 'operators_greater_than'
   | 'operators_less_than_or_equal'
+  | 'operators_less_than'
   | 'operators_multiply'
   | 'operators_not_equal'
-  | 'operators_parenthesis'
   | 'operators_parenthesis_left'
   | 'operators_parenthesis_right'
+  | 'operators_parenthesis'
   | 'operators_subtract'
+  | 'outbound_campaign_manager'
   | 'product_analyst'
   | 'product_console'
   | 'product_editor'
   | 'product_explorer'
-  | 'outbound_campaign_manager'
   | 'resize_northeast'
   | 'resize_northwest'
   | 'resize_southeast'
   | 'resize_southwest'
-  | 'rules'
   | 'rules_insert'
   | 'rules_select'
-  | 'script'
+  | 'rules'
   | 'script_macro'
   | 'script_sql'
   | 'script_stored_procedure'
+  | 'script'
   | 'segmentation'
-  | 'server'
   | 'server_changed'
   | 'server_edit'
   | 'server_foreign'
   | 'server_lab'
   | 'server_search'
   | 'server_synced'
+  | 'server'
   | 'sftp'
-  | 'table'
   | 'table_changed'
   | 'table_edit'
   | 'table_foreign'
   | 'table_lab'
   | 'table_search'
   | 'table_synced'
+  | 'table'
   | 'teradata'
   | 'text_analysis'
   | 'undock'
