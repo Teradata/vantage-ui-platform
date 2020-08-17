2e3c11472d1f4dfc1d7fcf11cccd44bdc810f386
Phil Dominguez <felips.110@gmail.com>
2020-08-17 08:54:45 -0700

    feat(): long form small scale styles and markdown update (#117)
    * feat: long form small scale styles and markdown update
    
    * fix: missing import
    
    * feat: adding example
    
    * fix: lock issues
    
    * fix: adding blockquote styles
    
    * fix: margin outside of blockquote
    
    Co-authored-by: pd250030 <phillip.dominguez@teradata.com>

--- src/docs-ui/app/typography/typography.component.ts
+++ src/docs-ui/app/typography/typography.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IMarkdownNavigatorItem } from '@covalent/markdown-navigator';
 @Component({
   selector: 'app-typography',
   templateUrl: './typography.component.html',
   styleUrls: ['./typography.component.scss'],
 })
-export class TypographyComponent {}
+export class TypographyComponent {
+  items: IMarkdownNavigatorItem[] = [
+    {
+      title: 'Covalent',
+      description: 'Terdata UI Platform',
+      icon: 'whatshot',
+      url: 'https://raw.githubusercontent.com/ngx-translate/core/master/README.md',
+    },
+  ];
+}
